debda77cf9fe44723dd485073fa3842ec583342d
Bequiman <thales.medeiros@estudante.iftm.edu.br>
2022-09-01 19:28:23 -0300

    1. Criação do projeto de teste. 2. Criação da classe calculadora.

---
+++ projetoSprin/src/main/java/org/iftm/projetoSprin/ProjetoSprinApplication.java
@@ -0,0 +1,13 @@
+package org.iftm.projetoSprin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSprinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSprinApplication.class, args);
+	}
+
+}
