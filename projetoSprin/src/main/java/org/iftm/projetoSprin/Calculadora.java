84600d113de174ecef37e0ab25ed811212b163d1
Bequiman <thales.medeiros@estudante.iftm.edu.br>
2022-09-01 19:35:58 -0300

    Criando projeto de teste e classe calculadora.

--- projetoSprin/src/main/java/org/iftm/projetoSprin/Calculadora.java
+++ projetoSprin/src/main/java/org/iftm/projetoSprin/Calculadora.java
@@ -1,17 +1,17 @@
 package org.iftm.projetoSprin;
 
 public class Calculadora{
-public double somar (double num1, double num2){
-return 0.0;
-}
-public double subtrair (double num1, double num2){
-return 0.0;
-}
-public double multiplicar (double num1, double num2){
-return 0.0;
-}
-public double dividir (double num1, double num2){
-return 0.0;
+	public double somar (double num1, double num2){
+		return 0.0;
+	}
+	public double subtrair (double num1, double num2){
+		return 0.0;
+	}
+	public double multiplicar (double num1, double num2){
+		return 0.0;
+	}
+	public double dividir (double num1, double num2){
+		return 0.0;
 }
 }
 
