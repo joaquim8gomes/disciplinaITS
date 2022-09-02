cda2a2cbac97a7c8945f88fb67ec9b0fa8167e69
Bequiman <thales.medeiros@estudante.iftm.edu.br>
2022-09-01 21:31:16 -0300

    Criação do projeto e da classe Calculadora.

---
+++ primeiroSpringBoot/src/test/java/org/iftm/primeiroSpringBoot/entity/CalculadoraTest.java
@@ -0,0 +1,35 @@
+package org.iftm.primeiroSpringBoot.entity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+
+	@Test
+	public void testaSomaCorretaParaDoisInteirosPositivos() {
+		Calculadora calculadora = new Calculadora();		
+		int entradaNumero1 = 10;
+		int entradaNumero2 = 5;		
+		int resultadoEsperado = 15;
+		
+		calculadora.somar(entradaNumero1, entradaNumero2);
+		int resultadoObtido = calculadora.getResultado();
+		
+		Assertions.assertEquals(resultadoEsperado, resultadoObtido);		
+	}
+
+	@Test
+	public void testaSomaCorretaParaDoisInteirosNegativos() {
+		Calculadora calculadora = new Calculadora();		
+		int entradaNumero1 = -10;
+		int entradaNumero2 = 5;		
+		int resultadoEsperado = -5;
+		
+		calculadora.somar(entradaNumero1, entradaNumero2);
+		int resultadoObtido = calculadora.getResultado();
+		
+		Assertions.assertEquals(resultadoEsperado, resultadoObtido);		
+	}
+	
+	
+}
